eb9d77958c1b564b6e6a9b8cced434a9a76bae31
vijayGaikwad <vijay.gaikwad@thoughtworks.com>
2024-02-06 14:05:31 +0530

    [QP-Assessment] Add Postgres database support

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("com.h2database:h2")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.postgresql:postgresql")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
